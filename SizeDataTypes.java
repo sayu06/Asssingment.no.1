14f2510fb043be565f6d968d896b559a006b3593
sayu06 <109415353+sayu06@users.noreply.github.com>
2022-07-25 14:00:09 +0700

    Find Size of int, float, double and char in Your System

---
+++ SizeDataTypes.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author Sayali
+ */
+public class SizeDataTypes 
+{
+   public static void main(String args[])
+   {
+      System.out.println("Interger "+(Integer.SIZE/8)+"bytes");
+      System.out.println("Float "+(Float.SIZE/8)+"bytes");
+      System.out.println("Character "+(Character.SIZE/8)+"bytes");
+      System.out.println("Byte "+(Byte.SIZE/8)+"bytes");
+      System.out.println("Long "+(Long.SIZE/8)+"bytes");
+      System.out.println("Short "+(Short.SIZE/8)+"bytes");
+      System.out.println("Double "+(Double.SIZE/8)+"bytes");
+   } 
+}
+/*Program to find the size of data types.(int,float,char,double,short,byte)*/
