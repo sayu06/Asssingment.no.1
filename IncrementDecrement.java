bb95983c89a148b51cba36d07a7a25a151dac5dc
sayu06 <109415353+sayu06@users.noreply.github.com>
2022-07-25 14:34:48 +0700

    Increment ++ and Decrement -- Operator

---
+++ IncrementDecrement.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author Sayali
+ */
+public class IncrementDecrement {
+    public static void main(String args[]){
+        int a = 5;
+        System.out.println("Increment "+ ++a);
+         System.out.println("Decrement "+ --a);
+    }
+}
