f6c2c0fd0f0dd9a12ef04ab4ab453f45ae03aec2
sayu06 <109415353+sayu06@users.noreply.github.com>
2022-07-25 14:15:29 +0700

    Calculate Sum of Natural Numbers

---
+++ SumofNaturalno.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author Sayali
+ */
+public class SumofNaturalno
+{
+    public static void main (String args[])
+    {
+        int num = 10 , sum = 0;
+        for(int i=1;i<=num;i++)
+        {
+            sum = sum +i ;
+        }
+        System.out.println("Sum of 1 to 10 natural numbers = " +sum);
+    }
+    
+}
+/*Write a program to calculate the sum of first 10 natural numbers.*/
